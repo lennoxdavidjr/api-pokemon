28e83489753cd94c002b906d59968ad2d4803335
lennoxdavidjr <lennox.e.david@gmail.com>
2021-04-20 16:46:07 -0400

    chore: cleanup 1

--- src/controllers/pokemon.controller.ts
+++ src/controllers/pokemon.controller.ts
@@ -6,17 +6,7 @@ import {
   repository,
   Where,
 } from '@loopback/repository';
-import {
-  del,
-  get,
-  getModelSchemaRef,
-  param,
-  patch,
-  post,
-  put,
-  requestBody,
-  response,
-} from '@loopback/rest';
+import {get, getModelSchemaRef, param, patch, response} from '@loopback/rest';
 import {Pokemon} from '../models';
 import {PokemonRepository} from '../repositories';
 
@@ -31,26 +21,6 @@ export class PokemonController {
     @repository(PokemonRepository)
     public pokemonRepository: PokemonRepository,
   ) {}
-
-  @post('/pokemon')
-  @response(200, {
-    description: 'Pokemon model instance',
-    content: {'application/json': {schema: getModelSchemaRef(Pokemon)}},
-  })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Pokemon, {
-            title: 'NewPokemon',
-          }),
-        },
-      },
-    })
-    pokemon: Pokemon,
-  ): Promise<Pokemon> {
-    return this.pokemonRepository.create(pokemon);
-  }
 
   @get('/pokemon/count')
   @response(200, {
@@ -111,25 +81,6 @@ export class PokemonController {
     return this.pokemonRepository.getFavorites();
   }
 
-  @patch('/pokemon')
-  @response(200, {
-    description: 'Pokemon PATCH success count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Pokemon, {partial: true}),
-        },
-      },
-    })
-    pokemon: Pokemon,
-    @param.where(Pokemon) where?: Where<Pokemon>,
-  ): Promise<Count> {
-    return this.pokemonRepository.updateAll(pokemon, where);
-  }
-
   @patch('/pokemon/{id}/favorite')
   @response(200, {
     description: 'Mark your favorite Pokemon',
@@ -179,42 +130,5 @@ export class PokemonController {
     name: string,
   ): Promise<Pokemon | null> {
     return this.pokemonRepository.findByName(name.trim());
-  }
-
-  @patch('/pokemon/{id}')
-  @response(204, {
-    description: 'Pokemon PATCH success',
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Pokemon, {partial: true}),
-        },
-      },
-    })
-    pokemon: Pokemon,
-  ): Promise<void> {
-    await this.pokemonRepository.updateById(id, pokemon);
-  }
-
-  @put('/pokemon/{id}')
-  @response(204, {
-    description: 'Pokemon PUT success',
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() pokemon: Pokemon,
-  ): Promise<void> {
-    await this.pokemonRepository.replaceById(id, pokemon);
-  }
-
-  @del('/pokemon/{id}')
-  @response(204, {
-    description: 'Pokemon DELETE success',
-  })
-  async deleteById(@param.path.string('id') id: string): Promise<void> {
-    await this.pokemonRepository.deleteById(id);
   }
 }
